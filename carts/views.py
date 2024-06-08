f42394c4c2967fe7e63dd35528ed20419bf0874f
Alireza <alireza.bayrami@gmail.com>
2024-06-08 20:09:35 +0330

    new commit

--- carts/views.py
+++ carts/views.py
@@ -6,6 +6,7 @@ from .models import Order, OrderItem
 from django.contrib import messages
 import time
 
+
 def cart(request):
     select_user_order_main_basket = Order.objects.filter(user=request.user.pk, is_complete=False).first()
     order_items = OrderItem.objects.filter(order=select_user_order_main_basket).all()
@@ -22,7 +23,7 @@ def cart(request):
         "order_total_price": order_total_price,
         "tax": tax,
         "price_with_tax": price_with_tax,
-        
+
     }
     return render(request, 'carts/cart.html', context)
 
@@ -39,17 +40,17 @@ def add_product_to_basket(request):
             already_purchased_this_item.quantity += 1
             already_purchased_this_item.save()
         else:
-            OrderItem.objects.create(product=product_exists, 
-                                    order=has_open_basket, 
-                                    price=product_exists.price, 
-                                    color=color, 
-                                    size=size)        
+            OrderItem.objects.create(product=product_exists,
+                                     order=has_open_basket,
+                                     price=product_exists.price,
+                                     color=color,
+                                     size=size)
     else:
         new_basket = Order.objects.create(user=request.user)
-        OrderItem.objects.create(product=product_exists, 
-                                 order=new_basket, 
-                                 price=product_exists.price, 
-                                 color=color, 
+        OrderItem.objects.create(product=product_exists,
+                                 order=new_basket,
+                                 price=product_exists.price,
+                                 color=color,
                                  size=size)
     messages.success(request, "Product added to your basket successfully.")
     time.sleep(20)
@@ -58,7 +59,8 @@ def add_product_to_basket(request):
 
 def remove_product_from_basket(request, product_slug):
     select_user_open_basket = Order.objects.filter(user=request.user.pk, is_complete=False).first()
-    is_product_exsits_in_user_basket = OrderItem.objects.filter(order=select_user_open_basket, product__slug=product_slug).first()
+    is_product_exsits_in_user_basket = OrderItem.objects.filter(order=select_user_open_basket,
+                                                                product__slug=product_slug).first()
     if is_product_exsits_in_user_basket:
         is_product_exsits_in_user_basket.delete()
     return redirect(reverse("cart-page"))
@@ -66,15 +68,18 @@ def remove_product_from_basket(request, product_slug):
 
 def increase_product_quantity(request, product_slug):
     select_user_open_basket = Order.objects.filter(user=request.user.pk, is_complete=False).first()
-    is_product_exsits_in_user_basket = OrderItem.objects.filter(order=select_user_open_basket, product__slug=product_slug).first()
+    is_product_exsits_in_user_basket = OrderItem.objects.filter(order=select_user_open_basket,
+                                                                product__slug=product_slug).first()
     if is_product_exsits_in_user_basket:
         is_product_exsits_in_user_basket.quantity += 1
         is_product_exsits_in_user_basket.save()
     return redirect(reverse("cart-page"))
 
+
 def decrease_product_quantity(request, product_slug):
     select_user_open_basket = Order.objects.filter(user=request.user.pk, is_complete=False).first()
-    is_product_exsits_in_user_basket = OrderItem.objects.filter(order=select_user_open_basket, product__slug=product_slug).first()
+    is_product_exsits_in_user_basket = OrderItem.objects.filter(order=select_user_open_basket,
+                                                                product__slug=product_slug).first()
     if is_product_exsits_in_user_basket:
         is_product_exsits_in_user_basket.quantity -= 1
         if is_product_exsits_in_user_basket.quantity < 1:
@@ -82,15 +87,9 @@ def decrease_product_quantity(request, product_slug):
             return redirect(reverse("cart-page"))
         is_product_exsits_in_user_basket.save()
     return redirect(reverse("cart-page"))
-    
-
 
 
 def show_user_basket(request):
     select_user_order_main_basket = Order.objects.filter(user=request.user.pk, is_complete=False).first()
     order_items = OrderItem.objects.filter(order=select_user_order_main_basket).all()
     return order_items
-    
-    
-    
-
